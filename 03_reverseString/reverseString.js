bd082f058b2f3885f011fcaa138805b8efef669d
wtyo <prasetyosuwignyo8@gmail.com>
2022-12-13 21:00:53 +0700

    npm attribute 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    // create an empty array
+    const revA = []
+    const length = str.length - 1
 
+    // looping to reserve the words
+    for (let i = length; i >= 0; i -= 1) {
+        revA.push(str[i]) // insert index by index
+    }
+
+    return revA.join("")
 };
 
 // Do not edit below this line
