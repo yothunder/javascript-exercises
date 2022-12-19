5ba696fef692167caafb386c0d2d169b449be4c9
wtyo <prasetyosuwignyo8@gmail.com>
2022-12-19 14:53:09 +0700

    finish task

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    return array.filter(element => !args.includes(element)) // tanda ! untuk mengambil nilai yang berkebalikan
 };
 
 // Do not edit below this line
