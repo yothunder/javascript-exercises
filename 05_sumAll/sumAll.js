aa0b8a18ba52db271d41715603e08c600f2d5abe
wtyo <prasetyosuwignyo8@gmail.com>
2022-12-19 20:22:18 +0700

    finish task

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (isNaN(a) || typeof b !== "number") { // more accurate using the later format by typeof
+        return 'ERROR'
+    }
+    if (a < 0 || b < 0) {
+        return 'ERROR'
+    }
+    let min = Math.min(a, b)
+    let max = Math.max(a,b)
+    let sum = 0
+    for (let i = min; i <= max; i += 1) {
+        sum += i
+    }
+    return sum
 };
 
 // Do not edit below this line
