ae538b1e8f7792ddfefe981496fa9a438811fff3
wtyo <prasetyosuwignyo8@gmail.com>
2022-12-20 17:12:28 +0700

    finish task

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let sum = 0
+	a.forEach(i => {
+    sum += i
+  })
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let multiply = 1
+	a.forEach(i => {
+    multiply *= i
+  })
+  return multiply
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a,b)
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let ans=1;
+  for (let i = 2; i <= a; i++)
+      ans = ans * i;
+  return ans;
 };
 
 // Do not edit below this line
