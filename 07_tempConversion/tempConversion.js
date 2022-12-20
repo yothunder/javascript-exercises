0819a2bb769c275498d360c4305d376b1876cdca
wtyo <prasetyosuwignyo8@gmail.com>
2022-12-20 16:49:15 +0700

    finish task

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(t) {
+  let tc = (5/9)*(t-32)
+  let tcf = tc.toFixed(1) // round to 1 decimal by converting to string 
+  return parseFloat(tcf) // convert string to number again
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(t) {
+  let tc = (9/5*t)+32
+  let tcf = tc.toFixed(1)
+  return parseFloat(tcf)
 };
 
 // Do not edit below this line
