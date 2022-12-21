da2cd9daf22e91c16aa4f1d839cc95d19784e329
wtyo <prasetyosuwignyo8@gmail.com>
2022-12-21 10:10:26 +0700

    finish task

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let l = 0
+    let h = str.length
+    str = str.toLowerCase()
+    while(l <= h) {
+        let getS = str[l]
+        let getE = str[h]
 
+        if (!(getS >= "a" && getS <= "z")) {
+            l += 1
+        } else if (!(getE >= "a" && getE <= "z")) {
+            h -= 1
+        } else if (getS == getE) {
+            l += 1
+            h -= 1
+        } else {
+            return false
+        }
+    }
+    return true
 };
 
 // Do not edit below this line
