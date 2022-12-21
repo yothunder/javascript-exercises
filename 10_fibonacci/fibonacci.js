1d9c3cd7d3bee953f68e3768eef67d32404b9e27
wtyo <prasetyosuwignyo8@gmail.com>
2022-12-21 10:44:15 +0700

    finish task

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = parseInt(n)
+    if (n < 0) {
+        return "OOPS"
+    } else if (n <= 1) {
+        return n;
+    }
+    return fibonacci(n-1) + fibonacci(n-2);
 };
 
 // Do not edit below this line
